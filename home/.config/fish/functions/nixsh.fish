f2c888ffa57a812e4554f9ea29336c3d3668481d
yangrui <lubui.com@gmail.com>
2026-04-14 11:59:03 +0800

    feat: up

--- home/.config/fish/functions/nixsh.fish
+++ home/.config/fish/functions/nixsh.fish
@@ -4,8 +4,10 @@ function nixsh
         return 1
     end
 
-    # set -g PATH (cached-nix-shell --expr "with (import $HOME/nix/default.nix {}).pkgs; mkShell {packages = [ $argv ];}" --run 'echo $PATH')
-    set -l path (nix shell --extra-experimental-features "nix-command flakes" --impure --expr "with (import $HOME/nix/default.nix {}).pkgs; buildEnv {name=\"my-shell\";paths=[$argv];}" --command printenv PATH)
-    and set -g PATH "$path"
-    and echo "$PATH"
+    set -l packages
+    for pkg in $argv
+        set -a packages "nixpkgs#$pkg"
+    end
+
+    nix shell --extra-experimental-features "nix-command flakes" $packages -c fish
 end
