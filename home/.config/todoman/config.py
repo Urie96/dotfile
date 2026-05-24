355aba829b50a5726f038acf5a4f9124c7dd13bd
yangrui <lubui.com@gmail.com>
2026-05-24 18:27:39 +0800

    feat: up

---
+++ home/.config/todoman/config.py
@@ -0,0 +1,29 @@
+# Path to calendar directories synced by vdirsyncer
+path = "~/.calendars/*"
+
+# Default list for new todos (must match a calendar name)
+default_list = "personal"
+
+# Date format
+date_format = "%Y-%m-%d"
+
+# Time format
+time_format = "%H:%M"
+
+# Default priority for new todos (1-9, where 1 is highest)
+default_priority = 5
+
+# Default due date offset for new todos (e.g., "3d" for 3 days)
+# default_due = "1d"
+
+# Start of the week (0 = Monday, 6 = Sunday)
+startofweek = 0
+
+# Color output
+color = "auto"
+
+# Show completed todos by default
+show_completed = False
+
+# Humanize dates (show "tomorrow" instead of date)
+humanize = True
