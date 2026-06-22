2cdd82f1b59aa3f15630b64e95d6e0d03d2dfed5
yangrui <lubui.com@gmail.com>
2026-06-22 16:00:03 +0800

    feat: up

--- home/.config/yazi/init.lua
+++ home/.config/yazi/init.lua
@@ -50,4 +50,22 @@ require('full-border'):setup { type = ui.Border.ROUNDED }
 
 -- require('yamb'):setup {}
 
-require('folder-rules'):setup()
+-- Folder-specific sort rules (replaces folder-rules plugin)
+ps.sub('ind-sort', function(opt)
+  local cwd = cx.active.current.cwd
+  if cwd:ends_with('Downloads') then
+    opt.by, opt.reverse, opt.dir_first = 'mtime', true, false
+  else
+    opt.by, opt.reverse, opt.dir_first = 'mtime', true, true
+  end
+  return opt
+end)
+
+-- Custom app title (replaces deprecated title_format in yazi.toml)
+ps.sub('ind-app-title', function(args)
+  local cwd = tostring(cx.active.current.cwd)
+  local home = os.getenv 'HOME'
+  if home then cwd = cwd:gsub('^' .. home, '~') end
+  args.value = 'YA:' .. cwd
+  return args
+end)
