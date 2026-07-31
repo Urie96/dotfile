f7d9ed60133ade7ab2c74ff5bf5e27799f9f1857
yangrui <lubui.com@gmail.com>
2026-07-31 14:16:58 +0800

    feat: up

--- home/.config/nvim/lua/plugins/optional.lua
+++ home/.config/nvim/lua/plugins/optional.lua
@@ -75,21 +75,19 @@ Config.on_keys({ '<leader>cr' }, setup_codediff)
 Config.on_cmd({ 'CodeDiff' }, setup_codediff)
 
 Config.on_packchanged('rime.nvim', { 'update', 'install' }, function(d)
-  vim.notify('Building rime.so', 'info')
   local res = vim.system({ './build.sh' }, { cwd = d.path }):wait()
   if res.code ~= 0 then
-    vim.notify('Failed to build rime.so: ' .. res.stderr, 'error')
+    vim.notify('Failed to build rimeshim.so: ' .. res.stderr, 'error')
   else
-    vim.notify('Done', 'info')
+    vim.notify('Build rimeshim.so done', 'info')
   end
-end, 'Build rime.so')
+end, 'Build rimeshim.so')
 
 Config.on_keys({ '<C-x>' }, 'i', function()
   vim.pack.add { 'https://github.com/Urie96/rime.nvim' }
   local rime = require 'rime'
   rime.setup {
     shared_data_dir = '~/.config/rime',
-    user_data_dir = '~/.config/rime',
   }
 
   vim.keymap.set('i', '<C-x>', function() rime.toggle() end, { desc = 'Toggle Rime' })
