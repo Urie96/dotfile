5db1ab540cf27378cd6f59258952b8ead34c5f54
yangrui.0 <yangrui.0@bytedance.com>
2026-08-05 14:10:34 +0800

    feat: up

--- home/.config/fish/conf.d/01_env.fish
+++ home/.config/fish/conf.d/01_env.fish
@@ -10,13 +10,13 @@ set -gx HF_ENDPOINT 'https://hf-mirror.com'
 set -gx LANG 'zh_CN.UTF-8'
 set -gx MANPAGER 'nvim +Man!'
 # set -gx PAGER '/nix/store/hkknbz4dhckmg55zlbpsypg68001rf32-custom_pager'
-set -gx PNPM_HOME ~/.local/share/pnpm/bin
+set -gx PNPM_HOME ~/.local/share/pnpm
 set -gx XDG_CACHE_HOME ~/.cache
 set -gx XDG_CONFIG_HOME ~/.config
 set -gx XDG_DATA_HOME ~/.local/share
 set -gx XDG_STATE_HOME ~/.local/state
 
-fish_add_path -g -m ~/bin ~/.local/bin ~/.nix-profile/bin /run/wrappers/bin /run/current-system/sw/bin /nix/var/nix/profiles/default/bin "$PNPM_HOME" "$HOME/.cargo/bin" /opt/homebrew/bin /opt/homebrew/sbin
+fish_add_path -g -m ~/bin ~/.local/bin ~/.nix-profile/bin /run/wrappers/bin /run/current-system/sw/bin /nix/var/nix/profiles/default/bin "$PNPM_HOME/bin" "$HOME/.cargo/bin" /opt/homebrew/bin /opt/homebrew/sbin
 
 if command -q flatnvim
     set -gx EDITOR flatnvim
